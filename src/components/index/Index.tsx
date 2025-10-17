c12dbf9ba84d7987ca516793887a3af279f32ed1
liushijun <liushijun@yeelight.com>
2025-10-17 16:39:48 +0800

    **Refactor:** Overhaul components and pages; optimize styles and structure for better maintainability.

--- src/components/index/Index.tsx
+++ src/components/index/Index.tsx
@@ -1,5 +1,3 @@
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 import Hero from "./Hero";
 import ProductShowcase from "./ProductShowcase";
 import CategoryGrid from "./CategoryGrid";
@@ -9,18 +7,14 @@ import Certificates from "./Certificates";
 
 const Index = () => {
   return (
-    <div className="min-h-screen">
-      <Header />
-      <main>
-        <Hero />
-        <ProductShowcase />
-        <CategoryGrid />
-        <PartnerBrands />
-        <Laboratory />
-        <Certificates />
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Hero />
+      <ProductShowcase />
+      <CategoryGrid />
+      <PartnerBrands />
+      <Laboratory />
+      <Certificates />
+    </>
   );
 };
 
